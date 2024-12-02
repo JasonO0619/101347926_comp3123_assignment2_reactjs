d62d4f423c50762f288b800299825d3e625b4d94
Jason <jayopoku19@gmail.com>
2024-12-01 23:11:15 -0800

    Small adjustments to User

--- src/components/User/SignUp.js
+++ src/components/User/SignUp.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import API from '../../Axios/API';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
